80e36c62808cc8af302dd8727a07c0e5e3e0ac05
0x0geek <0x0geek@gmail.com>
2023-07-21 05:36:28 -0400

    Commit for unit test, integration test

--- test/BaseSetup.sol
+++ test/BaseSetup.sol
@@ -44,4 +44,52 @@ contract BaseSetup is Test {
         fraig = users[5];
         vm.label(fraig, "Fraig");
     }
+
+    function getCarolProof() internal pure virtual returns (bytes32[] memory) {
+        bytes32[] memory carolProof = new bytes32[](6);
+        carolProof[
+            0
+        ] = 0x411e41cf526f9b4828f6f061c006384b3f94d8fce89a584920eea74822442900;
+        carolProof[
+            1
+        ] = 0x28ada27893d936924fa9e8cfebced2bf6e7a798d7ac2e83bcccf21529736d043;
+        carolProof[
+            2
+        ] = 0x9a2e42a5ebbb8ba90ea2dccb4a5b0760b70531b2b96b04398a80e777cc714cab;
+        carolProof[
+            3
+        ] = 0x060fc39ac014e73e7231eaa79053ef49eec49e3a554d1f68aa0f384cdfcb5599;
+        carolProof[
+            4
+        ] = 0x568e566600331f0f34d18cb63c8d22c4878d32d5dedcc12330980f0b98742cdc;
+        carolProof[
+            5
+        ] = 0x09ec4188996816741bb7669dc0236b4364c2aa3f50e355553036883b1e33615f;
+
+        return carolProof;
+    }
+
+    function getFraigProof() internal pure virtual returns (bytes32[] memory) {
+        bytes32[] memory fraigProof = new bytes32[](6);
+        fraigProof[
+            0
+        ] = 0x5c5566ed9ca278962336d8318093681848d3b15d943f9984481273cd7a0bcdfe;
+        fraigProof[
+            1
+        ] = 0x08e9d837d1874b31c14ba278d75a503e6e5a31653e74203255973ff6a0957581;
+        fraigProof[
+            2
+        ] = 0x9fd6d6c685b39b8973429ce39ea7d8499ce3d7c1113b27a87d6bf18ff217e0fe;
+        fraigProof[
+            3
+        ] = 0x060fc39ac014e73e7231eaa79053ef49eec49e3a554d1f68aa0f384cdfcb5599;
+        fraigProof[
+            4
+        ] = 0x568e566600331f0f34d18cb63c8d22c4878d32d5dedcc12330980f0b98742cdc;
+        fraigProof[
+            5
+        ] = 0x09ec4188996816741bb7669dc0236b4364c2aa3f50e355553036883b1e33615f;
+
+        return fraigProof;
+    }
 }
--- test/Lottery.t.sol
+++ test/Lottery.t.sol
@@ -5,15 +5,17 @@ import "forge-std/Test.sol";
 import {console} from "forge-std/console.sol";
 
 import "@openzeppelin-upgrade/contracts/proxy/ClonesUpgradeable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 import "../src/LotteryV1.sol";
 import "../src/LotteryV2.sol";
+import "./utils/Math.sol";
 import {BaseSetup} from "./BaseSetup.sol";
 import {UUPSProxy} from "./utils/UUPSProxy.sol";
-import {MerkleTree} from "./MerkleTree.sol";
 
 contract LotteryTest is BaseSetup {
     using ClonesUpgradeable for address;
+    using MathCeil for uint256;
 
     LotteryV1 public lottery1;
     LotteryV2 public lottery2;
@@ -25,8 +27,11 @@ contract LotteryTest is BaseSetup {
 
     uint8 internal constant PROTOCOL_FEE = 10;
     uint8 internal constant RENTTOKEN_FEE = 80;
-    uint256 internal constant RENT_AMOUNT = 1;
+    uint256 internal constant RENT_AMOUNT = 3;
     uint256 internal constant WINNER_COUNT = 10;
+
+    bytes32 internal constant ROOT_HASH =
+        0x3f00740cab856a945742c57efce40128df32394d0d7e8732fe46a36da2e40d1a;
 
     function setUp() public virtual override {
         BaseSetup.setUp();
@@ -40,79 +45,343 @@ contract LotteryTest is BaseSetup {
             PROTOCOL_FEE,
             RENTTOKEN_FEE,
             RENT_AMOUNT,
-            WINNER_COUNT
+            WINNER_COUNT,
+            address(david)
         );
     }
 
     function testCanInitialize() public {
         assertEq(wrappedProxy1.rentAmount(), RENT_AMOUNT);
+        assertEq(wrappedProxy1.rentTokenFee(), RENTTOKEN_FEE);
+        assertEq(wrappedProxy1.protocolFee(), PROTOCOL_FEE);
+        assertEq(wrappedProxy1.numberOfWinners(), WINNER_COUNT);
     }
 
     function testCanUpgrade() public {
+        // Create Lottery V2 contract for upgrade.
         lottery2 = new LotteryV2();
         wrappedProxy1.upgradeTo(address(lottery2));
 
         // re-wrap the proxy
         wrappedProxy2 = LotteryV2(address(proxy));
 
+        // Once upgrade is finished, check Lottery V1's variable is still alive
         assertEq(wrappedProxy2.rentAmount(), RENT_AMOUNT);
 
+        // Once upgrade is finished, check if the new function is working fine.
         wrappedProxy2.setPrizeAmount(200);
         assertEq(wrappedProxy2.prizeAmount(), 200);
+        assertEq(wrappedProxy2.rentAmount(), RENT_AMOUNT);
+        assertEq(wrappedProxy2.rentTokenFee(), RENTTOKEN_FEE);
         assertEq(wrappedProxy2.protocolFee(), PROTOCOL_FEE);
+        assertEq(wrappedProxy2.numberOfWinners(), WINNER_COUNT);
     }
 
-    // function testGenerateMerkleTree() public {
-    //     uint256 length = users.length;
+    function testStartLottery() public {
+        // Alice is not onwer and tries to start lottery, should revert
+        vm.startPrank(alice);
+        vm.expectRevert("Ownable: caller is not the owner");
+        wrappedProxy1.startLottery(ROOT_HASH);
+        vm.stopPrank();
 
-    //     address[] memory whitelistUsers = new address[](length);
+        // Onwer tries to start lottery with invalid root hash address
+        vm.expectRevert(LotteryV1.NotValidRootHash.selector);
+        wrappedProxy1.startLottery(bytes32(0));
 
-    //     for (uint256 i; i != length; ++i) {
-    //         whitelistUsers[i] = users[i];
-    //     }
+        wrappedProxy1.startLottery(ROOT_HASH);
+    }
 
-    //     MerkleTree merkleTree = new MerkleTree(whitelistUsers);
+    function testDecideWinner() public {
+        // Alice is not onwer and tries to start lottery, should revert
+        vm.startPrank(alice);
+        vm.expectRevert("Ownable: caller is not the owner");
+        wrappedProxy1.decideWinner();
+        vm.stopPrank();
 
-    //     assertEq(
-    //         merkleTree.getRoot(),
-    //         0x8dbc2bdf5655b07f404dd2fb7dc9a85ecf58436257795789a8548e228881b36d
-    //     );
-    // }
+        // Onwer tries to decide winner, but lottery is not started yet, should revert
+        vm.expectRevert(LotteryV1.LotteryNotInDepositPeriod.selector);
+        wrappedProxy1.decideWinner();
 
-    // function getMerkleTreeRoot() private returns (bytes32) {
-    //     uint256 length = users.length;
+        // Onwer sets number of winner
+        wrappedProxy1.setNumberOfWinners(WINNER_COUNT);
 
-    //     address[] memory whitelistUsers = new address[](length);
+        // Onwer start lottery
+        wrappedProxy1.startLottery(ROOT_HASH);
 
-    //     for (uint256 i; i != length; ++i) {
-    //         whitelistUsers[i] = users[i];
-    //     }
+        // Onwer tries to decide winner, but no participants in the lottery, should revert
+        vm.expectRevert(LotteryV1.NoParticipantsInLottery.selector);
+        wrappedProxy1.decideWinner();
 
-    //     MerkleTree merkleTree = new MerkleTree(whitelistUsers);
-    //     bytes32 rootHash = merkleTree.getRoot();
+        bytes32[] memory proof = new bytes32[](0);
 
-    //     assertEq(
-    //         rootHash,
-    //         0x8dbc2bdf5655b07f404dd2fb7dc9a85ecf58436257795789a8548e228881b36d
-    //     );
+        // Alice participate in the current lottery with 20 Ether
+        vm.prank(alice);
+        wrappedProxy1.joinLottery{value: 20 ether}(proof);
 
-    //     return rootHash;
-    // }
+        // Bob participate in the current lottery with 10 Ether
+        vm.prank(bob);
+        wrappedProxy1.joinLottery{value: 10 ether}(proof);
 
-    // function testIntegration() public {
-    //     bytes32 rootHash = getMerkleTreeRoot();
+        // Owner sets number of winner 10, but the number of participator is 2 now, should revert
+        vm.expectRevert(LotteryV1.InvalidNumberOfWinners.selector);
+        wrappedProxy1.decideWinner();
 
-    //     wrappedProxy1.startLottery(rootHash);
+        // Frag joins to lottery and he is a whitelist user, so that he doesn't send Ether.
+        vm.prank(fraig);
+        wrappedProxy1.joinLottery(getFraigProof());
 
-    //     vm.startPrank(alice);
-    //     bytes32[] memory data = new bytes32[](2);
-    //     data[
-    //         0
-    //     ] = 0x0bd8c9c2ec12639173b58f716b945909988d63d0d61b99ce5356b80b1443238c;
-    //     data[
-    //         1
-    //     ] = 0x7a71af3d21c3235c7f81420b8427f9f166d08e5b1e11d837eaf15d00e05c80fe;
-    //     wrappedProxy1.joinLottery(data);
-    //     vm.stopPrank();
-    // }
+        uint256 depositAmount;
+
+        // N members participate in the current lottery
+        for (uint256 i = 5; i != users.length; ++i) {
+            uint256 randomNumber = getRandom();
+            depositAmount = randomNumber % (1 * ETHER_DECIMAL);
+            vm.prank(users[i]);
+            wrappedProxy1.joinLottery{value: depositAmount}(proof);
+
+            skip(3600);
+        }
+
+        wrappedProxy1.decideWinner();
+    }
+
+    function testClaimReward() public {
+        vm.startPrank(alice);
+        // Alice tries to enter the current lottery, but lottery is not started yet, therfore should revert
+        vm.expectRevert(LotteryV1.NotSelectedWinners.selector);
+        wrappedProxy1.claimReward();
+        vm.stopPrank();
+
+        initLotteryConf();
+
+        // Bob borrows alice's NFT ticket
+        vm.prank(bob);
+        wrappedProxy1.rentTicket{value: RENT_AMOUNT}(address(alice));
+
+        // Owner decided winner
+        wrappedProxy1.decideWinner();
+
+        uint256 balanceBefore = address(bob).balance;
+
+        // Bob claims reward
+        vm.prank(bob);
+        wrappedProxy1.claimReward();
+
+        uint256 bobReward = address(bob).balance - balanceBefore;
+
+        // Bob's lender is Alice and He claims reward
+        balanceBefore = address(alice).balance;
+        vm.prank(alice);
+        wrappedProxy1.claimReward();
+
+        uint256 aliceReward = address(alice).balance - balanceBefore;
+
+        uint256 totalReward = aliceReward + bobReward;
+
+        // check if Bobs claimed 20% NFT ticket's reward and Alice claimed 80% of it.
+        assertEq(bobReward, (totalReward * 20) / 100);
+        assertEq(aliceReward, (totalReward * 80).divCeil(100));
+
+        /// warning claim test in not break period
+    }
+
+    function testJoinLottery() public {
+        bytes32[] memory proof = new bytes32[](0);
+
+        // Alice tries to enter the current lottery, but lottery is not started yet, therfore should revert
+        vm.startPrank(alice);
+        vm.expectRevert(LotteryV1.LotteryNotInDepositPeriod.selector);
+        wrappedProxy1.joinLottery{value: 10}(proof);
+        vm.stopPrank();
+
+        // Onwer set number of winners as 2
+        wrappedProxy1.setNumberOfWinners(2);
+
+        // Owner start the new  lottery
+        wrappedProxy1.startLottery(ROOT_HASH);
+
+        // Alice is not a whitelist user,but tries to participate in the lottery without deposited amount and proof.
+        vm.startPrank(alice);
+        vm.expectRevert(LotteryV1.NotWhitelistedUser.selector);
+        wrappedProxy1.joinLottery(proof);
+
+        // Alice participate in the current lottery
+        wrappedProxy1.joinLottery{value: 10}(proof);
+
+        skip(1000);
+
+        // Alice deposit amount in the current lottery again
+        wrappedProxy1.joinLottery{value: 20}(proof);
+        vm.stopPrank();
+
+        // Bob participate in the current lottery again
+        vm.prank(bob);
+        wrappedProxy1.joinLottery{value: 30}(proof);
+
+        // Carol participate in the current lottery again
+        vm.prank(carol);
+        wrappedProxy1.joinLottery{value: 15}(proof);
+
+        // Carol participate in the current lottery again
+        vm.prank(edward);
+        wrappedProxy1.joinLottery{value: 5}(proof);
+
+        // Owner decides winner
+        wrappedProxy1.decideWinner();
+
+        // Carol tries to participate in the current lottery, but deposit amount is already ended, should revert
+        vm.expectRevert(LotteryV1.LotteryNotInDepositPeriod.selector);
+        vm.prank(carol);
+        wrappedProxy1.joinLottery{value: 10}(proof);
+    }
+
+    function testRentTicket() public {
+        bytes32[] memory proof = new bytes32[](0);
+
+        // Alice tries to enter the current lottery, but lottery is not started yet, therfore should revert
+        vm.startPrank(alice);
+        vm.expectRevert(LotteryV1.LotteryNotInDepositPeriod.selector);
+        wrappedProxy1.rentTicket{value: 10}(address(alice));
+        vm.stopPrank();
+
+        // Owner start the new  lottery
+        wrappedProxy1.startLottery(ROOT_HASH);
+
+        // Alice participate in the current lottery
+        vm.startPrank(alice);
+        wrappedProxy1.joinLottery{value: 10}(proof);
+
+        // Alice tries to borrow NFT ticket with 2 Ether, but rent amount is 3, should revert
+        vm.expectRevert(LotteryV1.InsufficientRentAmount.selector);
+        wrappedProxy1.rentTicket{value: 2}(address(alice));
+
+        // Alice tries to borrow his NFT ticket with 3 Ether, but can't borrow his own ticket, should revert
+        vm.expectRevert(LotteryV1.NotRentableForOwner.selector);
+        wrappedProxy1.rentTicket{value: RENT_AMOUNT}(address(alice));
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        // Bob borrows carol's NFT ticket with 3 Ether, but Carol hasn't a NFT ticket for him, so should revert
+        vm.expectRevert(LotteryV1.InvalidTicketForOwner.selector);
+        wrappedProxy1.rentTicket{value: RENT_AMOUNT}(address(carol));
+
+        // Bob borrows alice's NFT ticket
+        wrappedProxy1.rentTicket{value: RENT_AMOUNT}(address(alice));
+        vm.stopPrank();
+    }
+
+    function testWithdrawProtocolReward() public {
+        initLotteryConf();
+
+        // Owner decides lottery's winner.
+        wrappedProxy1.decideWinner();
+
+        // Alice tries to withdraw the protocol reward and he is not a developer of contract, should revert
+        vm.expectRevert(LotteryV1.InvalidDevAddress.selector);
+        vm.prank(alice);
+        wrappedProxy1.withdrawProtocolReward();
+
+        uint256 balanceBefore = address(david).balance;
+
+        // David is a developer, possible to withdraw protocol reward
+        vm.prank(david);
+        wrappedProxy1.withdrawProtocolReward();
+
+        assertGe(address(david).balance, balanceBefore);
+    }
+
+    function testWithdrawProtocolRewardWithAmount() public {
+        initLotteryConf();
+
+        // Owner decides lottery's winner.
+        wrappedProxy1.decideWinner();
+
+        // Alice tries to withdraw the protocol reward and he is not a developer of contract, should revert
+        vm.expectRevert(LotteryV1.InvalidDevAddress.selector);
+        vm.prank(alice);
+        wrappedProxy1.withdrawProtocolReward(10 ether);
+
+        uint256 balanceBefore = address(david).balance;
+
+        // David is a developer, possible to withdraw protocol reward
+        vm.prank(david);
+        wrappedProxy1.withdrawProtocolReward(10 ether);
+
+        assertEq(address(david).balance, balanceBefore + 10 ether);
+    }
+
+    function testSetRentTokenFee() public {
+        // Alice tries to set rent token fee, but he is not a owner, should rever
+        vm.expectRevert("Ownable: caller is not the owner");
+        vm.prank(alice);
+        wrappedProxy1.setRentTokenFee(RENTTOKEN_FEE);
+
+        // Lottery is already started, hence should revert
+        wrappedProxy1.setRentTokenFee(RENTTOKEN_FEE);
+
+        // Owner starts new lottery.
+        wrappedProxy1.startLottery(ROOT_HASH);
+
+        // Lottery is already started, hence should revert
+        vm.expectRevert(LotteryV1.LotteryNotEnded.selector);
+        wrappedProxy1.setRentTokenFee(RENTTOKEN_FEE);
+    }
+
+    function initLotteryConf() private {
+        bytes32[] memory aliceProof = new bytes32[](0);
+
+        // Owner starts new lottery.
+        wrappedProxy1.startLottery(ROOT_HASH);
+
+        // Alice joins to lottery with 10 Ether.
+        vm.prank(alice);
+        wrappedProxy1.joinLottery{value: 10 ether}(aliceProof);
+
+        // Carol joins to lottery and he is a whitelist user, so that he doesn't send Ether.
+        vm.prank(carol);
+        wrappedProxy1.joinLottery(getCarolProof());
+
+        // Alice deposit 20 Ether again
+        vm.prank(alice);
+        wrappedProxy1.joinLottery{value: 20 ether}(aliceProof);
+
+        // Frag joins to lottery and he is a whitelist user, so that he doesn't send Ether.
+        vm.prank(fraig);
+        wrappedProxy1.joinLottery(getFraigProof());
+
+        uint256 depositAmount;
+
+        for (uint256 i = 10; i != users.length; ++i) {
+            uint256 randomNumber = getRandom();
+            depositAmount = randomNumber % (1 * ETHER_DECIMAL);
+            vm.prank(users[i]);
+            wrappedProxy1.joinLottery{value: depositAmount}(aliceProof);
+
+            skip(360);
+        }
+    }
+
+    function testIntegration() public {
+        initLotteryConf();
+
+        // Owner decides lottery's winner.
+        wrappedProxy1.decideWinner();
+
+        // Alice claims his reward
+        vm.prank(alice);
+        wrappedProxy1.claimReward();
+    }
+
+    function getRandom() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        ROOT_HASH,
+                        block.timestamp,
+                        block.difficulty
+                    )
+                )
+            );
+    }
 }
