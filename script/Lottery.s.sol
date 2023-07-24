0c095b41ed3e353d3cad788139d698f1a1562173
0x0geek <0x0geek@gmail.com>
2023-07-24 12:23:23 -0400

    Changed Deploy script code

--- script/Lottery.s.sol
+++ script/Lottery.s.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
+pragma solidity 0.8.19;
 
 import "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 import "../src/LotteryV1.sol";
+import "../src/LotteryV2.sol";
 import {UUPSProxy} from "../test/utils/UUPSProxy.sol";
 
 contract Lottery is Script {
@@ -11,14 +13,19 @@ contract Lottery is Script {
     uint256 internal constant RENT_AMOUNT = 3;
     uint256 internal constant WINNER_COUNT = 10;
 
+    LotteryV1 wrappedProxy1;
+    LotteryV2 wrappedProxy2;
+    UUPSProxy proxy;
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        console.logUint(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
 
         LotteryV1 lottery1 = new LotteryV1();
-        UUPSProxy proxy = new UUPSProxy(address(lottery1), new bytes(0));
+        proxy = new UUPSProxy(address(lottery1), "");
 
-        LotteryV1 wrappedProxy1 = LotteryV1(address(proxy));
+        wrappedProxy1 = LotteryV1(address(proxy));
 
         wrappedProxy1.initialize(
             PROTOCOL_FEE,
@@ -27,6 +34,15 @@ contract Lottery is Script {
             WINNER_COUNT,
             0xEE86283a2DFCc1f52E86790e275e5b07b44A50E5
         );
+
+        LotteryV2 lottery2 = new LotteryV2();
+        wrappedProxy1.upgradeTo(address(lottery2));
+
+        // re-wrap the proxy
+        wrappedProxy2 = LotteryV2(address(proxy));
+
+        // Once upgrade is finished, check if the new function is working fine.
+        wrappedProxy2.setPrizeAmount(200);
 
         vm.stopBroadcast();
     }
