f1c6512452d264fc7f6de039a83b56506ed1b7ab
0x00dev <gabggnon@gmail.com>
2023-07-25 13:39:59 -0400

    Changed variable names in VRFConsumer

--- src/VRFConsumer.sol
+++ src/VRFConsumer.sol
@@ -6,23 +6,21 @@ import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 import "./interfaces/ILotteryV1.sol";
 
 contract VRFConsumer is VRFConsumerBaseV2 {
-    VRFCoordinatorV2Interface internal immutable COORDINATOR;
-    ILotteryV1 internal immutable lottery;
     address internal constant VRF_COORDINATOR_ADDRESS =
         0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
     bytes32 internal constant KEY_HASH =
         0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
 
+    uint16 internal constant REQUEST_CONFIRMATION = 5;
+    uint32 internal constant CALL_BACK_GAS_LIMIT = 2500000;
+    uint32 public numWords;
     uint64 internal subscriptionId;
-
-    //Mumbai has a max gas limit of 2.5 million,
-    uint32 callbackGasLimit = 2500000;
-
-    // The default is 3, but you can set this higher.
-    uint16 requestConfirmations = 5;
-    uint32 public numWords = 10;
     uint256 public requestId;
+
     address internal ownerAddress;
+
+    VRFCoordinatorV2Interface internal immutable coordinator;
+    ILotteryV1 internal immutable lottery;
 
     modifier onlyOwner() {
         require(msg.sender == ownerAddress);
@@ -34,7 +32,7 @@ contract VRFConsumer is VRFConsumerBaseV2 {
         address _parentAddress
     ) VRFConsumerBaseV2(VRF_COORDINATOR_ADDRESS) {
         lottery = ILotteryV1(_parentAddress);
-        COORDINATOR = VRFCoordinatorV2Interface(VRF_COORDINATOR_ADDRESS);
+        coordinator = VRFCoordinatorV2Interface(VRF_COORDINATOR_ADDRESS);
         ownerAddress = msg.sender;
         subscriptionId = _subscriptionId;
     }
@@ -42,11 +40,11 @@ contract VRFConsumer is VRFConsumerBaseV2 {
     // Assumes the subscription is funded sufficiently.
     function requestRandomWords() external onlyOwner {
         // Will revert if subscription is not set and funded.
-        requestId = COORDINATOR.requestRandomWords(
+        requestId = coordinator.requestRandomWords(
             KEY_HASH,
             subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
+            REQUEST_CONFIRMATION,
+            CALL_BACK_GAS_LIMIT,
             numWords
         );
     }
